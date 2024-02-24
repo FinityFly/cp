158aec1dea5fb32e5e1966b994ff0c188728704a
Finity <56236512+FinityFly@users.noreply.github.com>
2024-02-24 13:02:15 -0500

    Upload CSES problems

---
+++ Array_Description.cpp
@@ -0,0 +1,112 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx2")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+using vvll = vector<vector<ll> >;
+using vpll = vector<pair<ll, ll> >;
+using mll =  map<ll, ll>;
+
+#define FOR(i, s, e) for (ll i = (ll)s; i < (ll)e; i++)
+#define CFOR(i, s, e) for (ll i = (ll)s; i <= (ll)e; i++)
+#define RFOR(i, e, s) for (ll i = (ll)e; i > (ll)s; i--)
+#define TRAV(a, c) for (auto a : c)
+#define all(x) x.begin(), x.end()
+
+ll read(){ll s=0,f=1;char ch=getchar();while(ch<'0'||ch>'9'){if(ch=='-')f=-1;ch=getchar();}while(ch>='0'&&ch<='9'){s=(s<<3)+(s<<1)+ch-'0',ch=getchar();}return s*f;}
+template <class T>
+string to_string(T s, T e) {
+    if (s == e) return "[]";
+    string ret = "[" + to_string(*s++);
+    while (s != e) ret += ", " + to_string(*s++);
+    return ret + "]";
+}
+
+#define MM 100001
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+
+#define FASTIO ;
+// #define PRECISION ;
+// #define FILE ;
+
+#define SINGLE ;
+// #define MULTIPLE ;
+// #define GOOGLE ;
+
+ll n, m;
+ll dp[100001][101];
+
+void solve() {
+    cin >> n >> m;
+    ll arr[n];
+    FOR (i, 0, n) cin >> arr[i];
+    if (arr[0] == 0) {
+        FOR (i, 0, m+1) dp[0][i] = 1;
+    } else {
+        dp[0][arr[0]] = 1;
+    }
+    FOR (i, 1, n) {
+        if (arr[i] == 0) {
+            for (int j = 1; j <= m; j++) {
+                for (int k : {j-1,j,j+1}) {
+                    if (k >= 1 && k <= m) {
+                        dp[i][j] += dp[i-1][k];
+                        dp[i][j] %= MOD;
+                    }
+                }
+            }
+        } else {
+            for (int k : {arr[i]-1, arr[i], arr[i]+1} ) {
+                if (k >= 1 && k <= m) {
+                    dp[i][arr[i]] += dp[i-1][k];
+                    dp[i][arr[i]] %= MOD;
+                }
+            }
+        }
+    }
+    ll ans=0;
+    FOR (i, 1, m+1) {
+        ans += dp[n-1][i];
+        ans %= MOD;
+    }
+    cout << ans << "\n";
+}
+
+int main() {
+#ifdef FASTIO
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+#endif
+#ifdef PRECISION
+    cout << fixed << setprecision(10);
+#endif
+#ifdef FILE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+#ifdef SINGLE
+    solve();
+#endif
+#ifdef MULTIPLE
+    ll t;
+    cin >> t;
+    for (ll i = 0; i < t; i++) {
+        solve();
+    }
+#endif
+#ifdef GOOGLE
+    ll t;
+    cin >> t;
+    for (ll i = 1; i <= t; i++) {
+        cout << "Case #" << i << ": ";
+        solve();
+    }
+#endif
+}
---
+++ Book_Shop.cpp
@@ -0,0 +1,91 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx2")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+using vvll = vector<vector<ll> >;
+using vpll = vector<pair<ll, ll> >;
+using mll =  map<ll, ll>;
+
+#define FOR(i, s, e) for (ll i = (ll)s; i < (ll)e; i++)
+#define CFOR(i, s, e) for (ll i = (ll)s; i <= (ll)e; i++)
+#define RFOR(i, e, s) for (ll i = (ll)e; i > (ll)s; i--)
+#define TRAV(a, c) for (auto a : c)
+#define all(x) x.begin(), x.end()
+
+ll read(){ll s=0,f=1;char ch=getchar();while(ch<'0'||ch>'9'){if(ch=='-')f=-1;ch=getchar();}while(ch>='0'&&ch<='9'){s=(s<<3)+(s<<1)+ch-'0',ch=getchar();}return s*f;}
+template <class T>
+string to_string(T s, T e) {
+    if (s == e) return "[]";
+    string ret = "[" + to_string(*s++);
+    while (s != e) ret += ", " + to_string(*s++);
+    return ret + "]";
+}
+
+#define MM 100001
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+
+#define FASTIO ;
+// #define PRECISION ;
+// #define FILE ;
+
+#define SINGLE ;
+// #define MULTIPLE ;
+// #define GOOGLE ;
+
+ll n, x;
+ll w[1001], v[1001];
+ll dp[100001];
+
+void solve() {
+    cin >> n >> x;
+    FOR (i, 0, n) cin >> w[i];
+    FOR (i, 0, n) cin >> v[i];
+    FOR (i, 0, n) {
+        RFOR (j, x, 0) {
+            if (w[i] <= j) {
+                dp[j] = max(dp[j], dp[j-w[i]] + v[i]);
+            }
+        }
+    }
+    cout << dp[x] << "\n";
+}
+
+int main() {
+#ifdef FASTIO
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+#endif
+#ifdef PRECISION
+    cout << fixed << setprecision(10);
+#endif
+#ifdef FILE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+#ifdef SINGLE
+    solve();
+#endif
+#ifdef MULTIPLE
+    ll t;
+    cin >> t;
+    for (ll i = 0; i < t; i++) {
+        solve();
+    }
+#endif
+#ifdef GOOGLE
+    ll t;
+    cin >> t;
+    for (ll i = 1; i <= t; i++) {
+        cout << "Case #" << i << ": ";
+        solve();
+    }
+#endif
+}
